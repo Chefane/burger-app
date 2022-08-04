1b0f684f00fea17f1394f2407f04c9fe5e23484f
Nosi <nosichefane@gmail.com>
2022-08-04 06:27:09 +0200

    final product

--- src/App.js
+++ src/App.js
@@ -1,112 +1,16 @@
 import React from 'react';
 import './App.css';
-
-
-
-
-//function for the heading
-
-function Heading(){
-
-  return <h2>Burger Demo</h2>;
-
-}
-
-//funtion for top bread
-function TopBread() {
- return (
- <div
-    style={{
-      display: 'block',
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingTop: '100px',
-      width: '70em',
-    }}
- >
- <button  className="topBread"></button>
-
- </div>
- );
-};
-
-
-
-//function for tomoto
-function Tomato(){
-  return (
-  
-  <div 
-  style={{
-    display: 'block',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '70em',
-    
-    
-  }}
-  ><button  className="tomato">Tomato</button></div> );
-
-  
-};
-
-//function for meat
-function Meat(){
-  return ( <div
-    style={{
-      display: 'block',
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: '70em',
-  
-    }}
-  ><button  className="meat">Meat</button></div>
-    );
-};
-
-//function for lattice
-function Lattice(){
-
-
-  return ( <div
-    style={{
-      display: 'block',
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: '70em',
-  
-    }}
-  ><button  className="lattice">Lattice</button></div>
-    );
-};
-
-//function for Bottom bread
-function BottomBread(){
-
-  return (
-  
-    <div
-    style={{
-      display: 'block',
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: '70em',
  
-    }}
-    ><button  className="bottomBread"></button></div>
-    );
-};
+import Burger from './components/burger';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Heading/>
-     <TopBread/>
-     <Tomato/>
-     <Meat/>
-     <Lattice/>
-     <BottomBread/>
- 
+    <div className="burgerContent">
+    
+    <Burger/>
 
     </div>
   );
